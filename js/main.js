892e9357d20f16374c7e9cdb0999fd8db2296600
Ben Ferreira <ben.ferreira@bos-dpc-00923.hq.verndalenet.com>
2016-04-04 13:16:11 -0400

    account adjustment

--- js/main.js
+++ js/main.js
@@ -163,3 +163,34 @@ $(".js-vertical-tab-accordion-heading").click(function(event) {
   $(".js-vertical-tab").removeClass("is-active");
   $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
 });
+
+(function (jQuery) {
+  jQuery.mark = {
+    jump: function (options) {
+      var defaults = {
+        selector: 'a.scroll-on-page-link'
+      };
+      if (typeof options == 'string') {
+        defaults.selector = options;
+      }
+
+      options = jQuery.extend(defaults, options);
+      return jQuery(options.selector).click(function (e) {
+        var jumpobj = jQuery(this);
+        var target = jumpobj.attr('href');
+        var thespeed = 1000;
+        var offset = jQuery(target).offset().top;
+        jQuery('html,body').animate({
+          scrollTop: offset
+        }, thespeed, 'swing');
+        e.preventDefault();
+      });
+    }
+  };
+})(jQuery);
+
+
+jQuery(function(){  
+  jQuery.mark.jump();
+});
+
